5981a6112a54bfe31db0cef6faf4876cca3874c5
yamaya-109 <yamaya.kai@compass-japan.com>
2025-04-02 09:42:35 +0900

    2025_04_02_kadai1_syusei

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-	@GetMapping("/hello")
-	public String hello()
-	{
-		return "hello World";
-	}
+    @GetMapping("/hello")
+    public String hello()
+    {
+       return "hello World";
+    }
 }
